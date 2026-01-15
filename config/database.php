8cc5ede590c9d61f367d2f129a7467dd9bbc3460
Juliana Dazza <julianad@unicauca.edu.co>
2026-01-15 14:38:38 -0500

    Conexión con la base de datos

---
+++ config/database.php
@@ -0,0 +1,25 @@
+<?php
+
+class Database {
+    private $host = "localhost";
+    private $db_name = "prueba_tecnica";
+    private $username = "root";
+    private $password = "root";
+    private $conn;
+
+    public function getConnection(){
+        $this->conn == null;
+
+        try{
+            $this->conn = new PDO( "mysql:host=" . $this->host . ";dbname=" . $this->db_name . ";charset=utf8mb4", $this->username, $this->password );
+            
+            $this->conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+            
+        } catch( PDOException $e ){
+            die("Error de conexión: " . $e->getMessage() );
+        }
+
+        return $this->conn;
+
+    }
+}
